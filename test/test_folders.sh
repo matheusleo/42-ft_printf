201d6496b2e3078a21b8d8b08064ab2d5c7b9dbe
matheusleo <matheusleotrab@gmail.com>
2022-06-13 21:41:37 -0300

    first commit
    add the basic files that will be necessary to the project

---
+++ test/test_folders.sh
@@ -0,0 +1,9 @@
+folders=(ft_print_char ft_print_str ft_print_int ft_print_dec ft_print_uint ft_print_hex ft_print_cap_hex ft_print_ptr)
+
+for i in "${folders[@]}"
+do
+	mkdir ${i}
+	cd ${i}
+	touch main.c
+	cd ..
+done
