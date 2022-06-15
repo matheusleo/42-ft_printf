e639112b404e24e875687c3a13664e898b82e3bb
matheusleo <matheusleotrab@gmail.com>
2022-06-15 10:50:04 -0300

    modify the test folders script
    remove the folder 'ft_print_cap_hex' - there is no more necessity of a function only responsible for printing an upper hex

--- test/test_folders.sh
+++ test/test_folders.sh
@@ -1,4 +1,4 @@
-folders=(ft_print_char ft_print_str ft_print_int ft_print_int ft_print_uint ft_print_hex ft_print_cap_hex ft_print_ptr)
+folders=(ft_print_char ft_print_str ft_print_int ft_print_int ft_print_uint ft_print_hex ft_print_ptr)
 
 for i in "${folders[@]}"
 do
