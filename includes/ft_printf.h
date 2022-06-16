1865a3f7918e943c1068936194234ff29e131d52
matheusleo <matheusleotrab@gmail.com>
2022-06-15 21:54:05 -0300

    add ft_printf
    add the main function ft_printf

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 20:52:43 by mleonard          #+#    #+#             */
-/*   Updated: 2022/06/15 11:14:43 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/06/15 19:29:04 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 # define FT_PRINTF_H
 # include "libft.h"
 # include <unistd.h>
+# include <stdarg.h>
 # define STDOUT 1
+
+// Main function
+int	ft_printf(const char *format, ...);
 
 // Functions to print the values to the STDOUT
 int	ft_print_char(int c);
---
+++ source/ft_printf.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/15 14:26:44 by mleonard          #+#    #+#             */
+/*   Updated: 2022/06/15 20:27:34 by mleonard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+static int	ft_parse_format_char(char indicator, va_list list)
+{
+	if (indicator == 'c')
+		return (ft_print_char(va_arg(list, int)));
+	if (indicator == 's')
+		return (ft_print_str(va_arg(list, char *)));
+	if (indicator == 'd')
+		return (ft_print_int(va_arg(list, int)));
+	if (indicator == 'i')
+		return (ft_print_int(va_arg(list, int)));
+	if (indicator == 'u')
+		return (ft_print_uint(va_arg(list, unsigned int)));
+	if (indicator == 'x')
+		return (ft_print_hex(va_arg(list, unsigned int), 0));
+	if (indicator == 'X')
+		return (ft_print_hex(va_arg(list, unsigned int), 1));
+	if (indicator == 'p')
+		return (ft_print_ptr(va_arg(list, void *)));
+	return (ft_print_char('%'));
+}
+
+int	ft_printf(const char *format, ...)
+{
+	int		format_len;
+	va_list	arg_list;
+
+	va_start(arg_list, format);
+	format_len = 0;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format_len += ft_parse_format_char(*(++format), arg_list);
+			format++;
+			continue ;
+		}
+		ft_putchar_fd(*format, STDOUT);
+		format++;
+		format_len++;
+	}
+	va_end(arg_list);
+	return (format_len);
+}
