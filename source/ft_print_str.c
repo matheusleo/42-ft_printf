22711231856cf7520ff96bcb61a384cd0fcde251
matheusleo <matheusleotrab@gmail.com>
2022-06-17 11:17:57 -0300

    fix ft_print_str
    adds condition for the edge case when the pointer received is NULL

--- source/ft_print_str.c
+++ source/ft_print_str.c
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 21:25:28 by mleonard          #+#    #+#             */
-/*   Updated: 2022/06/13 21:58:13 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/06/16 21:04:44 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_print_str(char *str)
 {
 	int	str_len;
 
+	if (!str)
+		str = "(null)";
 	str_len = ft_strlen(str);
 	ft_putstr_fd(str, STDOUT);
 	return (str_len);
