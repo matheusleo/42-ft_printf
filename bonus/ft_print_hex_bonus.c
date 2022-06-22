593a8890952a919152f25152e03c206076529ca6
matheusleo <matheusleotrab@gmail.com>
2022-06-22 10:29:47 -0300

    fix ft_print_hex_bonus
    fix problem with the alternative form from the %X indicator and modify the way that the flags are passed on the recursive function

--- bonus/ft_print_hex_bonus.c
+++ bonus/ft_print_hex_bonus.c
@@ -6,13 +6,13 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 21:48:55 by mleonard          #+#    #+#             */
-/*   Updated: 2022/06/21 21:49:52 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/06/22 10:27:33 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
 
-int	ft_print_hex(unsigned int nb, int is_upper, int is_alt)
+int	ft_print_hex(unsigned int nb, int is_upper, t_flags flags)
 {
 	char			*base;
 	int				nb_len;
@@ -23,19 +23,21 @@ int	ft_print_hex(unsigned int nb, int is_upper, int is_alt)
 	if (is_upper)
 	{
 		base = "0123456789ABCDEF";
-		if (is_alt)
-			nb_len += ft_print_str("0x");
+		if (flags.alt_form && nb != 0)
+			nb_len += ft_print_str("0X");
+		flags.alt_form = 0;
 	}
 	else
 	{
 		base = "0123456789abcdef";
-		if (is_alt)
+		if (flags.alt_form && nb != 0)
 			nb_len += ft_print_str("0x");
+		flags.alt_form = 0;
 	}
 	quoc = nb / ft_strlen(base);
 	remain = nb % ft_strlen(base);
 	if (quoc != 0)
-		nb_len += ft_print_hex(quoc, is_upper, FALSE);
+		nb_len += ft_print_hex(quoc, is_upper, flags);
 	ft_putchar_fd(base[remain], STDOUT);
 	return (nb_len);
 }
