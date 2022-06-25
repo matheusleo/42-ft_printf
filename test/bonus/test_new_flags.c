3ddff5f39541eb7e50db11d1f5a7e7f103dc0897
matheusleo <matheusleotrab@gmail.com>
2022-06-25 16:48:40 -0300

    finish tests
    save the temporary tests from the bonus part on the remote repo

--- test/bonus/test_new_flags.c
+++ test/bonus/test_new_flags.c
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 09:28:21 by mleonard          #+#    #+#             */
-/*   Updated: 2022/06/22 22:11:34 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/06/24 09:27:25 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ t_flags	ft_flag_parser(const char *str_flags)
 	flags.alt_form = FALSE;
 	flags.spaced_form = FALSE;
 	flags.signed_form = FALSE;
-	flags.left_padded = 0;
+	flags.dash_flag = 0;
 	flags.total_flags = 0;
 	while (!is_format_char(*str_flags))
 	{
@@ -66,7 +66,7 @@ t_flags	ft_flag_parser(const char *str_flags)
 			str_flags++;
 			while (!is_format_char(*str_flags) && !is_flag_char(*str_flags))
 			{
-				flags.left_padded = flags.left_padded * 10 + (*str_flags - '0');
+				flags.dash_flag = flags.dash_flag * 10 + (*str_flags - '0');
 				flags.total_flags++;
 				str_flags++;
 			}
@@ -75,18 +75,6 @@ t_flags	ft_flag_parser(const char *str_flags)
 		str_flags++;
 	}
 	return (flags);
-}
-
-// utils
-
-int	print_padding(int length, char c)
-{
-	int	counter;
-
-	counter = 0;
-	while (counter++ < length)
-		ft_putchar_fd(c, STDOUT);
-	return (counter);
 }
 
 // test functions
@@ -101,11 +89,11 @@ void	test_parser(void)
 	printf("used_flags.alt_form - %d\n", used_flags.alt_form);
 	printf("used_flags.spaced_form - %d\n", used_flags.spaced_form);
 	printf("used_flags.signed_form - %d\n", used_flags.signed_form);
-	printf("used_flags.left_padded - %d\n", used_flags.left_padded);
+	printf("used_flags.dash_flag - %d\n", used_flags.dash_flag);
 	printf("used_flags.total_flags - %d\n", used_flags.total_flags);
 }
 
-void	test_new_flags(void)
+void	test_flag_dash()
 {
 	char	*word;
 	int		nb;
@@ -113,76 +101,167 @@ void	test_new_flags(void)
 
 	word = "matheus";
 	nb = 42;
-	// Test #00 - Flag - (left_padded)
+	// Test #00 - Flag - (dash_flag)
 	set_color();
-	printf("\nTest #00 - Flag - (left_padded)\n\n");
+	printf("\nTest #00 - Flag - (dash_flag)\n\n");
 	reset_color();
+
+	printf("Test for c\n");
 	returned = printf("(%-20c)\n", *word);
 	printf("returned - %d\n\n", returned);
+	// returned = ft_printf("(%-20c)\n", *word);
+	// ft_printf("returned - %d\n\n", returned);
+
+	printf("Test for s\n");
 	returned = printf("(%-20s)\n", word);
 	printf("returned - %d\n\n", returned);
+	// returned = ft_printf("(%-20s)\n", word);
+	// ft_printf("returned - %d\n\n", returned);
+
+	printf("Test for p\n");
 	returned = printf("(%-20p)\n", word);
 	printf("returned - %d\n\n", returned);
+	// returned = ft_printf("(%-20p)\n", word);
+	// ft_printf("returned - %d\n\n", returned);
+
+	printf("Test for i\n");
 	returned = printf("(%-20i)\n", nb);
 	printf("returned - %d\n\n", returned);
+	// returned = ft_printf("(%-20i)\n", nb);
+	// ft_printf("returned - %d\n\n", returned);
+
+	printf("Test for u\n");
 	returned = printf("(%-20u)\n", nb);
 	printf("returned - %d\n\n", returned);
+	// returned = ft_printf("(%-20u)\n", nb);
+	// ft_printf("returned - %d\n\n", returned);
+
+	printf("Test for d\n");
 	returned = printf("(%-20d)\n", nb);
 	printf("returned - %d\n\n", returned);
+	// returned = ft_printf("(%-20d)\n", nb);
+	// ft_printf("returned - %d\n\n", returned);
+
+	printf("Test for x\n");
 	returned = printf("(%-20x)\n", nb);
 	printf("returned - %d\n\n", returned);
+	// returned = ft_printf("(%-20x)\n", nb);
+	// ft_printf("returned - %d\n\n", returned);
+
+	printf("Test for X\n");
 	returned = printf("(%-20X)\n", nb);
 	printf("returned - %d\n\n", returned);
+	// returned = ft_printf("(%-20X)\n", nb);
+	// ft_printf("returned - %d\n\n", returned);
+
 	printf("------\n");
+}
+
+void	test_flag_zero()
+{
+	char	*word;
+	int		nb;
+	int		returned;
+
+	word = "matheus";
+	nb = 42;
 
 	// Test #01 - Flag 0 (zero_padded)
 	set_color();
 	printf("\nTest #01 - Flag 0 (zero_padded)\n\n");
 	reset_color();
+
+	printf("Test for c\n");
 	returned = printf("(%020c)\n", *word);
 	printf("returned - %d\n\n", returned);
+
+	printf("Test for s\n");
 	returned = printf("(%020s)\n", word);
 	printf("returned - %d\n\n", returned);
+
+	printf("Test for p\n");
 	returned = printf("(%020p)\n", word);
 	printf("returned - %d\n\n", returned);
+
+	printf("Test for i\n");
 	returned = printf("(%020i)\n", nb);
 	printf("returned - %d\n\n", returned);
+
+	printf("Test for u\n");
 	returned = printf("(%020u)\n", nb);
 	printf("returned - %d\n\n", returned);
+
+	printf("Test for d\n");
 	returned = printf("(%020d)\n", nb);
 	printf("returned - %d\n\n", returned);
+
+	printf("Test for x\n");
 	returned = printf("(%020x)\n", nb);
 	printf("returned - %d\n\n", returned);
+
+	printf("Test for X\n");
 	returned = printf("(%020X)\n", nb);
 	printf("returned - %d\n\n", returned);
 	printf("------\n");
+
+}
+
+void	test_flag_dot(void)
+{
+	char	*word;
+	int		nb;
+	int		returned;
+
+	word = "matheus leonardo";
+	nb = 42;
 
 	// Test #02 - Flag . (precision)
 	set_color();
 	printf("\nTest #02 - Flag . (precision)\n\n");
 	reset_color();
+	printf("Test for c\n");
 	returned = printf("(%.20c)\n", *word);
 	printf("returned - %d\n\n", returned);
+
+	printf("Test for s\n");
 	returned = printf("(%.20s)\n", word);
 	printf("returned - %d\n\n", returned);
+
+	printf("Test for p\n");
 	returned = printf("(%.20p)\n", word);
 	printf("returned - %d\n\n", returned);
+
+	printf("Test for i\n");
 	returned = printf("(%.20i)\n", nb);
 	printf("returned - %d\n\n", returned);
+
+	printf("Test for u\n");
 	returned = printf("(%.20u)\n", nb);
 	printf("returned - %d\n\n", returned);
+
+	printf("Test for d\n");
 	returned = printf("(%.20d)\n", nb);
 	printf("returned - %d\n\n", returned);
+
+	printf("Test for x\n");
 	returned = printf("(%.20x)\n", nb);
 	printf("returned - %d\n\n", returned);
+
+	printf("Test for X\n");
 	returned = printf("(%.20X)\n", nb);
 	printf("returned - %d\n\n", returned);
+
+	printf("Test for .d\n");
+	returned = printf("(%.d)\n", nb);
+	printf("returned - %d\n\n", returned);
+
 	printf("------\n");
 }
-
 
 int	main(void)
 {
 	// test_parser();
-	// test_new_flags();
+	test_flag_dash();
+	test_flag_zero();
+	test_flag_dot();
 }
